c30054076c169db6e1be933755f879a52306ecc2
Berboa34 <s334200@studenti.polito.it>
2025-10-24 10:19:57 +0200

    Template DIRS and ensure dashboard.html path

--- scuolahub/settings.py
+++ scuolahub/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'scuolahub.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR  / 'scuolahub' / 'templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
