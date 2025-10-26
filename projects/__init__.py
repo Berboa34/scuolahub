ca8ae5626c6c37066aae3c904202685564235cb7
Berboa34 <s334200@studenti.polito.it>
2025-10-26 09:44:22 +0100

    Fix AppRegistryNotReady: no imports in apps.py; load signals in ready()

--- projects/__init__.py
+++
@@ -1,3 +0,0 @@
-default_app_config = 'projects.apps.ProjectsConfig'
-# oppure importa signals direttamente:
-# from . import signals
--- projects/apps.py
+++ projects/apps.py
@@ -1,17 +1,14 @@
+# projects/apps.py
 from django.apps import AppConfig
-
-# projects/signals.py
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import Profile
-
-@receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
-
 
 class ProjectsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'projects'
+
+    def ready(self):
+        # collega i signals SOLO quando le app sono pronte
+        try:
+            from . import signals  # noqa: F401
+        except Exception:
+            # se non usi i signals puoi anche lasciare pass
+            pass
