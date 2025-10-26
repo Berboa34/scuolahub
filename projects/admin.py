4453fb2f43a0e15ca28ad7a5a5ddacb090c20940
Berboa34 <s334200@studenti.polito.it>
2025-10-26 12:38:11 +0100

    Register School/Project/Profile in admin

--- projects/admin.py
+++ projects/admin.py
@@ -15,3 +15,9 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ("title", "school", "program", "status", "budget", "spent")
     list_filter = ("program", "status", "school")
     search_fields = ("title", "cup", "cig")
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "school", "role")
+    list_filter = ("school", "role")
+    search_fields = ("user__username", "user__email")
