f11a38851734509731b2ceaaec5689275a0ae012
Berboa34 <s334200@studenti.polito.it>
2025-11-12 16:03:39 +0100

    Remove db_check route

--- scuolahub/urls.py
+++ scuolahub/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path('accounts/login/',  auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page=reverse_lazy('login')), name='logout'),
 
-    # Debug DB opzionale
-    path('debug/db/', pviews.db_check, name='db_check'),
 ]
