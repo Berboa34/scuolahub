2a3b47e08acb83ea1604af0b738d8a795ab129b8
Berboa34 <s334200@studenti.polito.it>
2025-11-15 16:14:17 +0100

    Add Document model and documents section

--- projects/views.py
+++ projects/views.py
@@ -14,7 +14,7 @@ from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import School,  # aggiunto Document
+from .models import School # aggiunto Document
 
 
 
